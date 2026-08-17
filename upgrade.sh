646f89ea872b14854ead77357145905a1588e7e3
alvarosiles <242724234+alvarosiles@users.noreply.github.com>
2026-08-17 18:34:52 -0400

    update

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,2 @@
-# a
 wget --user=servisofts --password=Servisofts123. https://repo.servisofts.com/home/servisofts/libs/java/servisofts-java-service.jar
 mv ./servisofts-java-service.jar ./lib/servisofts-java-service.jar
