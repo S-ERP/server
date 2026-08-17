7ee68094c18aafaa79f752800713bc50d2c0a7b8
alvarosiles <242724234+alvarosiles@users.noreply.github.com>
2026-08-17 18:03:56 -0400

    Update alvaro_backend_backup .sh

--- alvaro_backend_backup .sh
+++ alvaro_backend_backup .sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 SSH_HOST="servisofts@192.168.2.5"
-REMOTE_BUILD_DIR="~/servicios/serp/entornos/serp"
+REMOTE_BUILD_DIR="/home/servisofts/servicios/serp/entornos/serp/servicios/serp"
 BACKUP_DATE=$(date +%Y-%m-%d_%H%M%S)
 
-echo "Respaldando build remoto..."
-ssh "$SSH_HOST" "cd $REMOTE_BUILD_DIR && cp -r build build_$BACKUP_DATE"
+echo "Respaldando server.jar remoto..."
+ssh "$SSH_HOST" "cd $REMOTE_BUILD_DIR && cp server.jar server.jar_$BACKUP_DATE"
 
 if [ $? -eq 0 ]; then
-    echo "Backup creado: $REMOTE_BUILD_DIR/build_$BACKUP_DATE"
+    echo "Backup creado: $REMOTE_BUILD_DIR/server.jar_$BACKUP_DATE"
 else
     echo "Error al crear el backup"
     exit 1
